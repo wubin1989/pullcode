7b379d2291aab8d08a00c9b20ada2a1ba18c38c2
wubin1989 <328454505@qq.com>
2022-07-04 11:02:33 +0800

    ...

--- dist/version.js
+++ dist/version.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.version = void 0;
-exports.version = 'v1.1.5';
+exports.version = 'v1.1.6';
 //# sourceMappingURL=version.js.map
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const version = 'v1.1.5';
+export const version = 'v1.1.6';
